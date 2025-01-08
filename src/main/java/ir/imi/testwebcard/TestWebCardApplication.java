01532a01201a5f4496ef3577431411feaa131519
Ashouri Omid <o.ashouri@imi.ir>
2025-01-08 14:51:58 +0330

    Initial commit

---
+++ src/main/java/ir/imi/testwebcard/TestWebCardApplication.java
@@ -0,0 +1,13 @@
+package ir.imi.testwebcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebCardApplication.class, args);
+    }
+
+}
